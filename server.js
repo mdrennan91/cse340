170a8b78fb837e3bb2992eef87771d57f3f7148a
mdrennan91 <mitcheldrennan@gmail.com>
2024-06-24 00:53:19 -0700

    Learning activity - content delivery mvc pattern, inventory route, controller, model

--- server.js
+++ server.js
@@ -10,7 +10,8 @@ const expressLayouts = require("express-ejs-layouts")
 const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
-
+const baseController = require("./controllers/baseController")
+const inventoryRoute = require("./routes/inventoryRoute.js")
 
 /* ***********************
  * View Engine and Templates
@@ -23,11 +24,12 @@ app.set("layout", "./layouts/layout") // not at views root
  * Routes
  *************************/
 app.use(static)
+// Inventory routes
+app.use("/inv", inventoryRoute)
+
 
 // Index Route
-app.get("/", function(req, res){
-  res.render("index", {title: "Home"})
-})
+app.get("/", baseController.buildHome)
 
 /* ***********************
  * Local Server Information
